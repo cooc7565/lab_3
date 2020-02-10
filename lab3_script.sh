80fcd0ca01e6ba2fc1b87e2a57f7a898e5f19ed8
cooc7565 <colton.oconnor@colorado.edu>
2020-02-09 22:37:30 -0700

    updated lab 3

--- lab3_script.sh
+++ lab3_script.sh
@@ -4,24 +4,27 @@
 
 #Problem 1 Code:
 #Make sure to document how you are solving each problem!
-echo "Input Filename: "
-read filename
-echo "Input search (showemail, showareacode, storeemail): "
-read expression
-a='showemail'
-b='showareacode'
+echo "Input Filename: "	#user puts in inteded file name "regex_practice.txt
+read filename		#reads file name
+echo "Input search (showemail, showareacode, storeemail): " #asks user what task to perform
+read expression		#reads task
+a='showemail'		#if user wants to display all emails
+b='showareacode'	#if user wants to display all phone numbers using 
 c='storeemail'
-if [ "$a" = "$expression" ]
+if [ "$a" = "$expression" ]	#displays emails found and gives total number
 then
 	egrep "@.*" $filename
 	echo "number of emails: "
 	grep -c "@.*" $filename
 fi
-if [ "$b" = "$expression" ] 
+if [ "$b" = "$expression" ] 	#displas 303 area code numbers found and displasy number
 then	
 	egrep "303.*" $filename
+	echo "number of phone numbers: "
+        grep -c "303." $filename
+
 fi
-if [ "$c" = "$expression" ]
+if [ "$c" = "$expression" ]	#saves emails to email_results.txt
 then
 	egrep "@.*" $filename >> email_results.txt
 fi
